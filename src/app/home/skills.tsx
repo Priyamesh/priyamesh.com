c3a3e7611c7cf67e068f60d49efda8bd570f8c24
Priyamesh <priyamesh28@gmail.com>
2024-09-06 17:32:51 +0530

    fixed skills

--- src/app/home/skills.tsx
+++ src/app/home/skills.tsx
@@ -4,7 +4,6 @@ export default function Skills() {
       <h1 className="text-lg font-semibold text-gray-700">Skills</h1>
       <p className="text-gray-500">
         React, Node.js, Django, Python, AWS, Docker, DSA and zealth to build.
-        something
       </p>
     </section>
   );
